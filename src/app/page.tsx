f646b608f9645503ea1500c239aff9024e11445c
kfarooq1981 <148969272+kfarooq1981@users.noreply.github.com>
2026-04-02 22:09:09 +0500

    Initial commit from Flare Codegen (API)

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import TicTacToe from '@/components/TicTacToe'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center p-4">
+      <TicTacToe />
+    </main>
+  )
+}
